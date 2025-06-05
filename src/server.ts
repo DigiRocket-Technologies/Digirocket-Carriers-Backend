9cdc93f6dcf4f2f3ec18abc2215fd611ed4ed64b
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2025-06-05 16:44:09 +0530

    cors and other setup

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,49 @@
-import express, { Request, Response } from 'express';
-import dotenv from "dotenv"
+// import express, { Request, Response } from 'express';
+// import dotenv from "dotenv"
 
-import contactRoutes from "./routes/contactRoutes"
+// import contactRoutes from "./routes/contactRoutes"
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT||5000
+
+// app.use(express.json());
+
+// app.use("/api/v1/contact",contactRoutes)
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello from TypeScript + Express!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors"; // Import the cors package
+import contactRoutes from "./routes/contactRoutes";
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT||5000
+const port = process.env.PORT || 5000;
+console.log(port, "port");
+
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your allowed origin
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
+app.use("/api/v1/contact", contactRoutes);
 
-app.use("/api/v1/contact",contactRoutes)
-
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from TypeScript + Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript + Express!");
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
